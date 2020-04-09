b52dc5e9a1f908b0801935c4eb4931fe63fb62ed
Alexandre-Abruzzese <alex.abruzzese96@gmail.com>
2020-04-09 10:37:52 +0200

    validate fonction done

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
-class EventController
+class EventController extends Controller
 {
     public function getAllEvents()
     {
