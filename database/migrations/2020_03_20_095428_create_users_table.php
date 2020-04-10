c424eafff7d98ad534b50da61c1f779febc2ae22
Arthur Cargnelli <Arthur-C.K@hotmail.fr>
2020-04-10 15:43:21 +0200

    fix test

--- database/migrations/2020_03_20_095428_create_users_table.php
+++ database/migrations/2020_03_20_095428_create_users_table.php
@@ -20,7 +20,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->string('api_token');
-            $table->integer('is_active');
+            $table->integer('is_active')->default(0);
             $table->timestamps();
         });
     }
